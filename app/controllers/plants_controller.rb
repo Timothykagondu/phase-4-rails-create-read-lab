e90a675868b729fe032b24d0d622812e89a648d8
TimothyKagondu <timothy.2k21@gmail.com>
2023-03-20 00:26:18 +0300

    completed assignment

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,23 @@
+class PlantsController < ApplicationController
+
+  def index
+    plants = Plant.all
+    render json: plants
+  end
+
+  def show
+    plant = Plant.find_by(id: params[:id])
+    render json: plant
+  end
+
+  def create
+    new_plant = Plant.create(params_data)
+    render json: new_plant, status: :created
+  end
+
+  private
+
+  def params_data
+    params.permit(:name, :image, :price)
+  end
+end
---
+++ app/models/plants.rb
@@ -0,0 +1,2 @@
+class plants < ApplicationRecord
+end
